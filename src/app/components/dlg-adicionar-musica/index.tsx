6d40137138a18f3613d78d6af6c902aba568c8f2
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2025-08-30 15:21:25 -0300

    feat: implementa dialog de adicionar musica

---
+++ src/app/components/dlg-adicionar-musica/index.tsx
@@ -0,0 +1,196 @@
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
+import {
+  Modal,
+  ScrollView,
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+import { useState } from "react";
+import { Picker } from "@react-native-picker/picker";
+import ButtonLigth from "../button-ligth";
+
+interface Props {
+  visible: string;
+  onClose: () => void;
+}
+
+export default function DlgAdicionarMusica({ visible, onClose }: Props) {
+  const [selectedTom, setSelectedTom] = useState("");
+
+  return (
+    <>
+      <Modal
+        visible={visible === 'addSong'}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={onClose}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContainer}>
+            <TouchableOpacity style={styles.iconClose} onPress={onClose}>
+              <MaterialIcons name="close" size={28} color="white" />
+            </TouchableOpacity>
+
+            <Text style={styles.modalTitle}>Adicionar música</Text>
+
+            <View style={{ gap: 10, flexDirection: "row" }}>
+              <View style={styles.searchBar}>
+                <TextInput
+                  style={styles.inputSearchBar}
+                  placeholder="Pesquisar"
+                />
+                <FontAwesome
+                  name="search"
+                  size={20}
+                  color="#000"
+                  style={styles.iconSearchBar}
+                />
+              </View>
+
+              <View style={styles.picker}>
+                <Picker
+                  onValueChange={(itemValue, itemIndex) =>
+                    setSelectedTom(itemValue)
+                  }
+                  selectedValue={selectedTom}
+                >
+                  <Picker.Item label="Tom" value="" enabled={false} />
+                  <Picker.Item label="C" value="C" />
+                  <Picker.Item label="C#" value="C#" />
+                  <Picker.Item label="D" value="D" />
+                  <Picker.Item label="D#" value="D#" />
+                  <Picker.Item label="E" value="E" />
+                  <Picker.Item label="F" value="F" />
+                  <Picker.Item label="F#" value="F#" />
+                  <Picker.Item label="G" value="G" />
+                  <Picker.Item label="G#" value="G#" />
+                  <Picker.Item label="A" value="A" />
+                  <Picker.Item label="A#" value="A#" />
+                  <Picker.Item label="B" value="B" />
+                </Picker>
+              </View>
+            </View>
+
+            <View style={{marginTop: 10}}>
+                <ButtonLigth title="Adicionar Música" onPress={() => {}}/>
+            </View>
+
+            <ScrollView
+              contentContainerStyle={styles.scrollContent}
+              showsVerticalScrollIndicator={false}
+            ></ScrollView>
+          </View>
+        </View>
+      </Modal>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#FEF5C8",
+    borderRadius: 11,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowRadius: 3,
+  },
+  row: {
+    flexDirection: "row",
+  },
+  titulo: {
+    color: "#000",
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.4)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContainer: {
+    width: "85%",
+    maxHeight: "70%",
+    backgroundColor: "#2c6b74",
+    borderRadius: 15,
+    padding: 20,
+    elevation: 10,
+    position: "relative",
+  },
+  iconClose: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    zIndex: 10,
+  },
+  modalTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 15,
+    color: "white",
+    textAlign: "center",
+  },
+  button: {
+    width: "100%",
+    height: 80,
+    backgroundColor: "#FEF5C8",
+    borderRadius: 15,
+    justifyContent: "space-between",
+    alignItems: "center",
+    shadowRadius: 3,
+    elevation: 5,
+    padding: 20,
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  buttonRow: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    gap: 5,
+  },
+  textModal: {
+    fontSize: 15,
+  },
+  scrollContent: {
+    paddingBottom: 20,
+  },
+  searchBar: {
+    flexDirection: "row",
+    flex: 2,
+    alignItems: "center",
+    backgroundColor: "#FEF5C8",
+    borderRadius: 11,
+    paddingHorizontal: 10,
+    height: 50,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  iconSearchBar: {
+    marginRight: 8,
+  },
+  inputSearchBar: {
+    flex: 1,
+    fontSize: 16,
+    color: "#000",
+  },
+  picker: {
+    flex: 1,
+    backgroundColor: "#FEF5C8",
+    borderRadius: 11,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
